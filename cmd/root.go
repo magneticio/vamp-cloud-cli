e0f68e6a1e277b65a3ca3dd463f3c06c5e0f9bde
Alessandro Valcepina <alessandro@magnetic.io>
2021-04-26 12:32:40 +0200

    increasing version

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,7 @@ import (
 const AppName string = "vamp-cloud-cli"
 
 // Version - version of the cli
-const Version string = "v1.0.5"
+const Version string = "v1.0.6"
 
 // ApiVersion - supported version of the api
 const ApiVersion string = "v1"
